2a524b49eec44c948c3200a9b96f082f44db82fc
DurbarTalluri <62018770+DurbarTalluri@users.noreply.github.com>
2021-08-07 03:03:44 +0530

    Update views.py

--- extra/views.py
+++ extra/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-from .models import Details
+#from .models import Details
 # Create your views here.
 def index(request):
     User=os.getenv("USERNAME")
