e968e0df09b59d70aef31abc5a88dfa9220cf04e
DurbarTalluri <62018770+DurbarTalluri@users.noreply.github.com>
2024-10-08 14:48:47 +0530

    Update apm-one-agent-linux.sh

--- apm-one-agent-linux.sh
+++ apm-one-agent-linux.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 exec >>"/home/durbar-11363/Documents/archive/ONEAGENT_INTEGRATION/test.log" 2>&1
-echo "AGENT RUN WITH ARGS $0 $@"
+echo "AGENT RUN WITH ARGS "$0" "$@""
+if [ "$(id -u)" -ne 0 ]; then
+    echo "OneAgent installer script is run without root privilege. Please run the script apm-one-agent-linux.sh with root privilege"
+    exit 1
+fi
