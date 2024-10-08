a41c4bd5dba0a11ee8b9413181bc215c313811cc
DurbarTalluri <62018770+DurbarTalluri@users.noreply.github.com>
2024-10-08 14:35:51 +0530

    Update apm-one-agent-linux.sh

--- apm-one-agent-linux.sh
+++ apm-one-agent-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec >>"$/home/durbar-11363/Documents/archive/ONEAGENT_INTEGRATION/test.log" 2>&1
+exec >>"/home/durbar-11363/Documents/archive/ONEAGENT_INTEGRATION/test.log" 2>&1
 echo "AGENT RUN WITH ARGS $0 $@"
