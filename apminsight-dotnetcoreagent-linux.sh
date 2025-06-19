ca1ade434285b9cbccec5ab0629caee6002844ef
DurbarTalluri <62018770+DurbarTalluri@users.noreply.github.com>
2025-06-19 20:09:08 +0530

    Update apminsight-dotnetcoreagent-linux.sh

--- apminsight-dotnetcoreagent-linux.sh
+++ apminsight-dotnetcoreagent-linux.sh
@@ -78,7 +78,6 @@ function InstallAgent() {
             fi
         else
             Print "Setting up the APM Insight .NET Core agent version $AgentVersion."
-	    exit 0
             CopyFiles "$agentPath"
             if [ "$OfflineInstall" = false ]; then
                 rm -rf dotnet_core_linux
