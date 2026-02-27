0419d0e7269feba5306016e8dc1205047de68bba
DurbarTalluri <62018770+DurbarTalluri@users.noreply.github.com>
2026-02-27 13:07:34 +0530

    Add files via upload

--- apminsight-auto-profiler.sh
+++ apminsight-auto-profiler.sh
@@ -186,7 +186,7 @@ ReadConfigFromArgs() {
 }
 
 CheckMandatoryConfigurations() {
-    if [ -z "$APMINSIGHT_LICENSEKEY" ] && [ -z "$APMINSIGHT_LICENSE_KEY" ]; then
+    if [ -z "$APMINSIGHT_LICENSE_KEY" ] && [ -z "$APMINSIGHT_LICENSEKEY" ]; then
         INSTALLATION_FAILURE_MESSAGE="No License key found. Please run the script again with proper License Key"
         exit 1
     elif [ -z "$SERVER_MONITOR_KEY" ]; then
